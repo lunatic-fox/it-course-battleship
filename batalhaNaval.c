ce07d0eb133f62a44c7fb2ebe132b005149a8efd
Lunatic Fox <joseliojrx25@gmail.com>
2025-12-18 09:47:12 -0300

    Commit nível aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,32 +3,28 @@
 
 int main() {
   int board[10][10]; // Tabuleiro
-  int shipVertical[SHIP_SIZE], shipHorizontal[SHIP_SIZE]; // Navio
 
   // Criação do tabuleiro
   for(int i = 0; i < 10; i++)
     for(int j = 0; j < 10; j++)
       board[i][j] = 0;
 
-  // Inserção de dados nos vetores dos navios
-  for(int i = 0; i < SHIP_SIZE; i++) {
-    shipVertical[i] = 3;
-    shipHorizontal[i] = 3;
-  }
+  // Inserção dos navios nos tabuleiro
+  // Navio horizontal: [2, 8][3, 8][4, 8]
+  for(int row = 2; row < 5; row++)
+    board[row][8] = SHIP_SIZE;
 
-  // Inserção dos navios nos tabuleiros
-  for(int row = 0; row < 10; row++)
-    for(int column = 0; column < 10; column++) {
-      // Navio vertical: [1, 4][2, 4][3, 4]
-      if (column == 4 && row >= 1 && row <= 3) 
-        for (int i = 0; i < SHIP_SIZE; i++)
-          board[column][row] = shipVertical[i];
-      
-      // Navio horizontal: [8, 2][8, 3][8, 4]
-      if (row == 8 && column >= 2 && column <= 4)
-        for (int i = 0; i < SHIP_SIZE; i++)
-          board[column][row] = shipHorizontal[i];
-    }
+  // Navio vertical: [4, 1][4, 2][4, 3]
+  for(int column = 1; column < 4; column++)
+    board[4][column] = SHIP_SIZE;
+  
+  // Navio diagonal: [6, 7][7, 6][8, 5]
+  for(int row = 6, column = 7; row < 9, column > 4; row++, column--)
+    board[row][column] = SHIP_SIZE;
+  
+  // Navio diagonal: [2, 4][3, 5][4, 6]
+  for(int row = 2, column = 4; row < 5, column < 7; row++, column++)
+    board[row][column] = SHIP_SIZE;
   
   // Exibição do tabuleiro
   for(int column = 0; column < 10; column++) {
